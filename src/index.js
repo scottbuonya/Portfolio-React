efb159256a5b90dc6e7fd6827a72a2ada33127fb
Scott Buonya <scottbuonya@gmail.com>
2020-10-02 17:53:50 -0400

    set up my links to other pages

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import reactDOM from 'react-dom';
 import App from './App';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import { BrowserRouter } from 'react-router-dom';
 
-reactDOM.render(<App />, document.getElementById('root'));
+reactDOM.render(
+  <BrowserRouter>
+<App />
+  </BrowserRouter>
+  , document.getElementById('root'));
