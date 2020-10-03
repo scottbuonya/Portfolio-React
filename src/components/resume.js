42765f59aa2b609fc6fd13cfa2969351456b11f7
Scott Buonya <scottbuonya@gmail.com>
2020-10-02 22:25:18 -0400

    finished my resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import pdf from '../resume/Resume.pdf'
+import { Card, CardTitle, CardActions, Button, CardText } from 'react-mdl'
 
 class Resume extends Component {
     render() {
         return(
-            <div><h1>Resume Page</h1></div>
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto', padding:'20px'}}>
+                    <CardTitle style={{ height: '176px', background: 'url(https://www.pngkey.com/png/detail/892-8928473_document-logo-rsum.png) center / cover'}} ></CardTitle>
+                    <CardText>
+                        Check Out my Resume!
+                    </CardText>
+                    <CardActions border>
+                        <Button colored><a href={pdf} target="_blank" rel="noopener noreferrer">Click here for My Resume!</a></Button>
+                        
+                    </CardActions>
+                </Card>
         )
     }
 }
