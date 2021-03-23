96efcf68668aaa7b699d4f9c035b6243f9c193e4
Scott Buonya <scottbuonya@gmail.com>
2021-03-23 14:41:56 -0400

    changes to resume file

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import pdf from '../resume/Scott-Buonya-2020.pdf'
+import pdf from '../resume/Scott-Buonya-Resume.pdf'
 import { Card, CardTitle, CardActions, Button, CardText } from 'react-mdl'
 
 class Resume extends Component {
     render() {
-        return(
-            <Card shadow={5} style={{minWidth: '450', margin: 'auto', padding:'20px'}}>
-                    <CardTitle style={{ height: '176px', background: 'url(https://www.pngkey.com/png/detail/892-8928473_document-logo-rsum.png) center / cover'}} ></CardTitle>
-                    <CardText>
-                        Check Out my Resume!
+        return (
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto', padding: '20px' }}>
+                <CardTitle style={{ height: '176px', background: 'url(https://www.pngkey.com/png/detail/892-8928473_document-logo-rsum.png) center / cover' }} ></CardTitle>
+                <CardText>
+                    Check Out my Resume!
                     </CardText>
-                    <CardActions border>
-                        <Button colored><a href={pdf} target="_blank" rel="noopener noreferrer">Click here for My Resume!</a></Button>
-                        
-                    </CardActions>
-                </Card>
+                <CardActions border>
+                    <Button colored><a href={pdf} target="_blank" rel="noopener noreferrer">Click here for My Resume!</a></Button>
+
+                </CardActions>
+            </Card>
         )
     }
 }
